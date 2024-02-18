01d5ce9c4c0b03b491ce14df98e50400f994a07d
Tri Thien Nguyen <anretrithien@gmail.com>
2024-02-18 16:41:46 +0000

    test

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -69,12 +69,12 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `Your Language Lounge`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
+        We are <strong className="text-stone-100">Your Language Lounge</strong>, currently working at{' '}
+        <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
         registrar and site builder.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
